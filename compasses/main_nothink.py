6dbba23f8cdc535c4445cdb8e2cadf7d97c2916a
Simone Angarano <simone.angarano@domyn.com>
2026-04-29 19:36:52 +0200

    add main compasses

---
+++ compasses/main_nothink.py
@@ -0,0 +1,23 @@
+import os
+
+from mmengine.config import read_base
+
+with read_base():
+    from .full_think_32k import (
+        models,
+        infer,
+        eval,
+        ruler_32k_datasets,
+        math_datasets,
+        humaneval_datasets,
+        LCB_datasets,
+        mgsm_datasets,
+    )
+
+datasets = [
+    *ruler_32k_datasets,
+    *math_datasets,
+    *humaneval_datasets,
+    *LCB_datasets,
+    *mgsm_datasets,
+]
